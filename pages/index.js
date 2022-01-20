2e8103d4b40ef3e39ffe76e2b8fa039b13f75330
univers3 <damianoher@hotmail.com>
2022-01-20 18:52:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Futbala</title>
+        <title>Welcome to Futbala</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
