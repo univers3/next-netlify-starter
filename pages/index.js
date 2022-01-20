7941c3b2e57e0d78454e53c07664f7445770d2a8
univers3 <damianoher@hotmail.com>
2022-01-20 18:58:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="🎉⚽️ Welcome to Futbala ⚽️🎉" />
+        <Header title="⚽️ Welcome to Futbala ⚽️" />
         <p className="description">
           The biggest soccer game is coming to town
         </p>
