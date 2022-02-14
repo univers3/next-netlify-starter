5a0c6cf4d027c522622be4cc4cde15d7d5627e09
univers3 <damianoher@hotmail.com>
2022-02-14 11:19:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container" style="background-image: linear-gradient(to left, rgba(255,255,255,0), rgba(220,220,220,1))">
       <Head>
         <title>Welcome to Futbala</title>
         <link rel="icon" href="/favicon.ico" />
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main>
         
-        <img src="futbala_main.png" alt="Futbala" />
+        <img src="futbala_main.png" alt="Futbala" style="width:50%" />
 
       </main>
     
